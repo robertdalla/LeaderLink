b4e63a62361b406c47ab80d983a661a2338ace80
robertdalla <robert.dalla@mojosoup.com.au>
2023-02-07 03:13:29 +1000

    completed migration to angular@9 All scripts work ok

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -32,6 +32,8 @@ import 'core-js/es/weak-map';
 import 'core-js/es/set';
 import 'core-js/es/array';
 
+import '@angular/localize/init';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
